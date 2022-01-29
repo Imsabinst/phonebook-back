9799f7653ae4491b1bfaaf4086b4b38b8183d250
sabin <sashre@utu.fi>
2022-01-29 14:06:48 +0200

    3.11 Phone directory and database, part 2

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ app.use(bodyParser.json());
 app.use(cors());
 const Person = require("./models/person");
 
+const logger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("checking");
+  next();
+};
+app.use(logger);
+
 const formatDetails = (person) => {
   const formattedDetails = { ...person._doc, id: person._id };
   delete formattedDetails._id;
@@ -21,14 +30,28 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-const logger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("checking");
-  next();
-};
-app.use(logger);
+app.get("/api/persons/:id", (request, response) => {
+  Person.findById(request.params.id).then((person) =>
+    response.json(formatDetails(person))
+  );
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  person.save().then((saveDetails) => {
+    response.json(formatDetails(saveDetails));
+  });
+});
+app.delete("/api/persons/:id", (request, response) => {
+  Person.findById(request.params.id).then((person) =>
+    response.json(person["_id" !== person])
+  );
+});
 
 const error = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
